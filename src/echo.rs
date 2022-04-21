60ce675d68e3bbdb0312ebd59680a394253bc879
Carlos Macasaet <github@carlos-m.net>
2022-04-20 18:20:16 -0700

    Move DTOs to shared library

--- src/echo.rs
+++ src/echo.rs
@@ -1,108 +1,12 @@
 extern crate serde;
 extern crate serde_with;
 
+mod lib;
+
+use crate::lib::Message;
+use crate::lib::MessageType;
 use std::io;
 use std::io::Write;
-
-use serde::{Deserialize, Serialize};
-use serde_with::skip_serializing_none;
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Message {
-    src: String,
-    dest: String,
-    body: MessageBody,
-}
-
-#[skip_serializing_none]
-#[derive(Deserialize, Serialize, Debug)]
-struct MessageBody {
-    #[serde(rename = "type")]
-    message_type: MessageType,
-
-    msg_id: Option<usize>,
-    in_reply_to: Option<usize>,
-
-    // init fields
-    node_id: Option<String>,
-    node_ids: Option<Vec<String>>,
-
-    // echo fields
-    echo: Option<String>,
-
-    // error fields
-    code: Option<u16>,
-    text: Option<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-#[allow(non_camel_case_types)]
-enum MessageType {
-    init,
-    init_ok,
-    error,
-    echo,
-    echo_ok,
-}
-
-impl Message {
-    fn init_ok(source: &str, destination: &str, message_id: usize, in_reply_to: usize) -> Self {
-        Self {
-            src: source.to_owned(),
-            dest: destination.to_owned(),
-            body: MessageBody {
-                message_type: MessageType::init_ok,
-                node_id: None,
-                node_ids: None,
-                echo: None,
-                code: None,
-                msg_id: Some(message_id),
-                in_reply_to: Some(in_reply_to),
-                text: None,
-            },
-        }
-    }
-
-    fn echo(
-        source: &str,
-        destination: &str,
-        message_id: usize,
-        in_reply_to: usize,
-        echo: &str,
-    ) -> Self {
-        Self {
-            src: source.to_owned(),
-            dest: destination.to_owned(),
-            body: MessageBody {
-                message_type: MessageType::echo_ok,
-                msg_id: Some(message_id),
-                in_reply_to: Some(in_reply_to),
-                node_id: None,
-                node_ids: None,
-                echo: Some(echo.to_owned()),
-                code: None,
-                text: None,
-            },
-        }
-    }
-
-    fn error(source: &str, destination: &str, in_reply_to: usize, code: u16, text: &str) -> Self {
-        Self {
-            src: source.to_owned(),
-            dest: destination.to_owned(),
-            body: MessageBody {
-                message_type: MessageType::error,
-                node_id: None,
-                node_ids: None,
-                echo: None,
-                code: Some(code),
-                msg_id: None,
-                in_reply_to: Some(in_reply_to),
-                text: Some(text.to_owned()),
-            },
-        }
-    }
-}
 
 fn main() {
     // Application State
---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Message {
+    pub src: String,
+    pub dest: String,
+    pub body: MessageBody,
+}
+
+#[skip_serializing_none]
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MessageBody {
+    #[serde(rename = "type")]
+    pub message_type: MessageType,
+
+    pub msg_id: Option<usize>,
+    pub in_reply_to: Option<usize>,
+
+    // init fields
+    pub node_id: Option<String>,
+    pub node_ids: Option<Vec<String>>,
+
+    // echo fields
+    pub echo: Option<String>,
+
+    // error fields
+    pub code: Option<u16>,
+    pub text: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[allow(non_camel_case_types)]
+pub enum MessageType {
+    init,
+    init_ok,
+    error,
+    echo,
+    echo_ok,
+}
+
+impl Message {
+    pub fn init_ok(source: &str, destination: &str, message_id: usize, in_reply_to: usize) -> Self {
+        Self {
+            src: source.to_owned(),
+            dest: destination.to_owned(),
+            body: MessageBody {
+                message_type: MessageType::init_ok,
+                node_id: None,
+                node_ids: None,
+                echo: None,
+                code: None,
+                msg_id: Some(message_id),
+                in_reply_to: Some(in_reply_to),
+                text: None,
+            },
+        }
+    }
+
+    pub fn echo(
+        source: &str,
+        destination: &str,
+        message_id: usize,
+        in_reply_to: usize,
+        echo: &str,
+    ) -> Self {
+        Self {
+            src: source.to_owned(),
+            dest: destination.to_owned(),
+            body: MessageBody {
+                message_type: MessageType::echo_ok,
+                msg_id: Some(message_id),
+                in_reply_to: Some(in_reply_to),
+                node_id: None,
+                node_ids: None,
+                echo: Some(echo.to_owned()),
+                code: None,
+                text: None,
+            },
+        }
+    }
+
+    pub fn error(
+        source: &str,
+        destination: &str,
+        in_reply_to: usize,
+        code: u16,
+        text: &str,
+    ) -> Self {
+        Self {
+            src: source.to_owned(),
+            dest: destination.to_owned(),
+            body: MessageBody {
+                message_type: MessageType::error,
+                node_id: None,
+                node_ids: None,
+                echo: None,
+                code: Some(code),
+                msg_id: None,
+                in_reply_to: Some(in_reply_to),
+                text: Some(text.to_owned()),
+            },
+        }
+    }
+}
