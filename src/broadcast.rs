2e5aa07cfa969ef454b07270fc5e2c36ef2ed2ca
Carlos Macasaet <github@carlos-m.net>
2022-05-03 22:54:33 -0700

    Create actual stateful request handler

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::RwLock;
 
 use crate::node::{AppError, Node};
-use crate::protocol::{Message, MessageType};
+use crate::protocol::{Message, MessageBody, MessageType};
 use crate::response::Response;
 use crate::server::{RequestHandler, Server};
 
@@ -17,29 +17,64 @@ struct BroadcastServer {
 }
 
 struct TopologyHandler {
-    broadcast_server: Arc<BroadcastServer>,
+    broadcast_server: RwLock<BroadcastServer>,
 }
 
 impl RequestHandler for TopologyHandler {
     fn handle_request(
         &self,
-        _node: &Node,
+        node: &Node,
         _in_reply_to: usize,
-        _request: &Message,
+        request: &Message,
     ) -> Result<Box<dyn Response>, AppError> {
-        todo!()
+        if request.body.topology.is_none() {
+            return Err(AppError::MissingField("body.topology".to_string()));
+        }
+        let topology = request.body.topology.clone().unwrap();
+        let neighbours = topology
+            .get(&node.read_node_id())
+            .map(Vec::clone)
+            .unwrap_or_default();
+        let mut server = self
+            .broadcast_server
+            .write()
+            .expect("broadcast server lock is poisoned");
+        server.neighbours = neighbours;
+        Ok(Box::new(TopologyOk {}))
+    }
+}
+
+struct TopologyOk;
+
+impl Response for TopologyOk {
+    fn to_message(&self, node: &Node, caller: &str, msg_id: usize, in_reply_to: usize) -> Message {
+        Message {
+            src: node.read_node_id(),
+            dest: caller.to_string(),
+            body: MessageBody {
+                message_type: MessageType::topology_ok,
+                msg_id: Some(msg_id),
+                in_reply_to: Some(in_reply_to),
+                node_id: None,
+                node_ids: None,
+                echo: None,
+                code: None,
+                text: None,
+                topology: None,
+            },
+        }
     }
 }
 
 fn main() {
-    let mut broadcast_server = BroadcastServer::default();
-    let mut topology_handler = TopologyHandler {
-        broadcast_server: Arc::new(broadcast_server),
+    let broadcast_server = BroadcastServer::default();
+    let topology_handler = TopologyHandler {
+        broadcast_server: RwLock::new(broadcast_server),
     };
 
     let mut handlers: HashMap<MessageType, Box<dyn RequestHandler>> = HashMap::new();
     handlers.insert(MessageType::topology, Box::new(topology_handler));
 
-    let mut server = Server::new(handlers);
+    let server = Server::new(handlers);
     server.run();
 }
--- src/echo.rs
+++ src/echo.rs
@@ -4,9 +4,9 @@ extern crate serde_with;
 use std::collections::HashMap;
 
 use crate::node::{AppError, Node};
-use crate::protocol::Message;
 use crate::protocol::MessageType;
-use crate::response::EchoResponse;
+use crate::protocol::{Message, MessageBody};
+use crate::response::Response;
 use crate::server::{RequestHandler, Server};
 use crate::AppError::MissingField;
 
@@ -31,4 +31,28 @@ fn echo(_node: &Node, _request_id: usize, request: &Message) -> Result<EchoRespo
     Ok(EchoResponse {
         text: request.body.echo.clone().unwrap(),
     })
+}
+
+pub struct EchoResponse {
+    pub text: String,
+}
+
+impl Response for EchoResponse {
+    fn to_message(&self, node: &Node, caller: &str, msg_id: usize, in_reply_to: usize) -> Message {
+        Message {
+            src: node.read_node_id(),
+            dest: caller.to_owned(),
+            body: MessageBody {
+                message_type: MessageType::echo_ok,
+                msg_id: Some(msg_id),
+                in_reply_to: Some(in_reply_to),
+                node_id: None,
+                node_ids: None,
+                echo: Some(self.text.clone()),
+                code: None,
+                text: None,
+                topology: None,
+            },
+        }
+    }
 }
